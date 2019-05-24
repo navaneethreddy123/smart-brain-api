853f87ecba4c57bfcd9d8608524631b12b1e32d8
navaneethreddy123 <30905263+navaneethreddy123@users.noreply.github.com>
2019-05-23 23:32:51 -0400

    updating database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-lively-96477',
-    user : 'nav',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
